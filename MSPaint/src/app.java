3566f6538ab2072aa8d98d027fcc74b65666e439
John Elnitski <jcelnitski@gmail.com>
2018-03-09 12:21:04 -0500

    GUI files for MSPaint

---
+++ MSPaint/src/app.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author John
+ */
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create the frame object
+        InitialFrame mjf = new InitialFrame();
+        
+    }
+    
+}
